6853d1de5ec5fed5f2d6fa233b89e9147f7d9992
michaelrychly <MichaelRychly@gmail.com>
2018-04-26 12:23:15 -0400

    categories implemented

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,10 @@ class Product < ActiveRecord::Base
   validates :quantity, presence: true
   validates :category, presence: true
 
+  def sold_out?
+    if quantity == 0
+      true
+    end
+  end
+
 end
