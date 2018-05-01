ea80b09f09feb814e843e214c245a430733fcc71
michaelrychly <MichaelRychly@gmail.com>
2018-05-01 13:43:33 -0400

    navigate to product detail test

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
+    1.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -17,13 +16,14 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "See details of one product" do
     # ACT
     visit root_path
 
+    click_on "Details »"
+    visit product_path(@category.products[0][:id])
     # DEBUG / VERIFY
     save_screenshot
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'article.product-detail', count: 1
   end
-
 end
